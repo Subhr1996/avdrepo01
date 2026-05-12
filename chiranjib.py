cf478b42c76e71abe88e2055ad474f4452720258
sarfaraj khan <sarfarajkhan0403@gmail.com>
2026-05-12 14:14:03 +0530

    chiranjib file edited

--- chiranjib.py
+++ chiranjib.py
@@ -1,7 +1,4 @@
-    def can_move(self, dice):
-        if self.finished:
-            return False
-        if self.pos == -1:
-            return dice == 6
-        remaining = 57 - self.pos  # steps to home (58 = center, but 52+5=57 is last home col)
-        return dice <= remaining + 1
+a=5
+b=7
+a,b=b,a
+print(a,b)
